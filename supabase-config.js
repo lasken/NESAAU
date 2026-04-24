eb92cecca3eac5b982515baf8851280462794e20
L-kenny <128179042+lasken@users.noreply.github.com>
2026-04-24 08:58:16 +0100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,3 @@
-
 var SUPABASE_URL = 'https://aurhyaspphwmaillbnaw.supabase.co';
 var SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1cmh5YXNwcGh3bWFpbGxibmF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3NTQ3MDMsImV4cCI6MjA5MjMzMDcwM30.nsIW9oY2ZpTonrlmWuk_u0Dr5Q-HEcred_dJazagUJ8';
 
